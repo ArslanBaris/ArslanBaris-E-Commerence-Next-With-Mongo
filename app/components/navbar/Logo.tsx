470ae7ca212ba08df2ebdaf9028d0c2e736864b9
ArslanBaris <arslan.bariss@outlook.com>
2024-05-13 22:26:59 +0300

    Layout changes

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,11 @@
+
+const Logo = () => {
+    return (
+        <div
+            className="bg-orange-700 px-2 py-1 rounded-md text-lg md:tex-2x cursor-pointer"
+        >Burada<span className="text-sm">.com</span>
+        </div>
+    )
+}
+
+export default Logo
---
+++ app/components/navbar/Search.tsx
@@ -0,0 +1,6 @@
+
+export const Search = () => {
+  return (
+    <div className="hidden md:flex flex-1">Search</div>
+  )
+}
